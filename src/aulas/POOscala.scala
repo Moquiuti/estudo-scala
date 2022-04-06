b40e2b58e43c0f0e6b0a0ad44e9ed22716dc4787
leandro <leandro.moquiuti@gmail.com>
2022-04-06 12:10:10 -0400

    Definição de métodos e cálculos matemáticos.

--- src/aulas/POOscala.scala
+++ src/aulas/POOscala.scala
@@ -1,5 +1,20 @@
 package aulas
 
-object POOscala  extends App {
-  println("Hello Word!")
+object POOscala extends App {
+
+  println("Praticando com métodos de calculos!");
+  println(soma(9, 9));
+  println(fib(9));
+  println(raiz(9));
+  println(fatorial(9));
+
+  def soma(a: Int, b: Int): Int = return a + b;
+
+  def fib(x: Int): Int = if (x == 1 || x == 2) 1 else fib(x - 1) + fib(x - 2)
+
+  def raiz(a: Double) = Math.sqrt(a);
+
+  def fatorial(x: Int): Int = if (x == 1) 1 else x * fatorial(x - 1)
+
+
 }
